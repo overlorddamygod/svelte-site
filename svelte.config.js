2cace1f74f952da05da894f736dbbaf4fd5892e7
overlorddamygod <overlord.damygod@gmail.com>
2021-08-03 15:21:03 +0545

    Adapter fix

--- svelte.config.js
+++ svelte.config.js
@@ -6,9 +6,8 @@ const config = {
   kit: {
     // hydrate the <div id="svelte"> element in src/app.html
     target: "#svelte",
+    adapter: vercel(),
   },
-
-  adapter: vercel(),
 
   preprocess: [
     preprocess({
