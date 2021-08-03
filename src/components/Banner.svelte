157f33c08e40f4fdc135daba18bb73b2a73bef03
overlorddamygod <overlord.damygod@gmail.com>
2021-08-03 16:44:51 +0545

    Responsive image

--- src/components/Banner.svelte
+++ src/components/Banner.svelte
@@ -1,6 +1,6 @@
-<div class="banner rounded-lg bg-blue-700 text-white p-14">
+<div class="banner rounded-lg bg-blue-700 text-white p-5 md:p-14">
     <h1 class="text-3xl font-bold">Hello World</h1>
-    <p class="text-gray-400 my-7 w-3/4 md:w-1/2">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem incidunt architecto id quod.</p>
+    <p class="text-gray-400 my-5 md:my-7 w-3/4 md:w-1/2">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem incidunt architecto id quod.</p>
     <a href="/contact"><button class="py-2 px-4 bg-green-500 text-white font-semibold rounded-lg shadow-md focus:outline-none hover:opacity-75">Contact Us</button></a>
 </div>
 
@@ -8,5 +8,6 @@
     .banner {
         background-image: url("../../../banner.jpg");
         background-color: #0A1816;
+        background-position: top;
     }
 </style>
--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -2,7 +2,7 @@
     export let name = "business.svg";
 </script>
 
-<div class="flex flex-col justify-center items-center text-center px-10 py-10 shadow-lg rounded-2xl cursor-pointer hover:bg-opacity-75">
+<div class="flex flex-col justify-center items-center text-center px-10 py-10 shadow-lg rounded-2xl cursor-pointer hover:bg-opacity-75 border">
     <img src={`/svg/${name}`} alt={name} class="block py-2">
     <slot/>
 </div>
